a5bec1fc22ae3838b3588293849e3d4fd0b9af43
antonioarjpi <antonioarjpi@gmail.com>
2021-09-15 15:07:39 -0300

    Person API

---
+++ src/main/java/com/york/springboot/PersonapiApplication.java
@@ -0,0 +1,15 @@
+package com.york.springboot;
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping ("/api/v1/people")
+public class PersonapiApplication {
+    @GetMapping
+    public String getBook(){
+        return "Api Test";
+    }
+}
